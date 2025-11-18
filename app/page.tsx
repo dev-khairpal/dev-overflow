8ecc4028c5d910926d595d4872411fd513b18fc0
Dev <devkhairpal@gmail.com>
2025-11-18 16:00:06 +0530

    shad cn ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const page = () => {
+  console.log("hello");
+  console.log("hello");
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      page
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
 
-export default page
+export default page;
